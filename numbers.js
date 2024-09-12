924c6ffbd701f5661510fc29d44d57727e31641d
alexamaegazo <alexamaegazo5@gmail.com>
2024-09-13 00:21:24 +0800

    Initial commit with numbers.js

---
+++ numbers.js
@@ -0,0 +1,20 @@
+const firstFavNum = 18;
+const secondFavNum = 29;
+
+const sum = firstFavNum + secondFavNum;
+console.log("addition:", sum);
+
+const dif =  firstFavNum - secondFavNum;
+console.log("subtraction:", dif);
+
+const product =  firstFavNum * secondFavNum;
+console.log("multiplication:", product);
+
+const quotient =  firstFavNum / secondFavNum;
+console.log("division:", quotient);
+
+const modulos =  firstFavNum % secondFavNum;
+console.log("modulos:", modulos);
+
+const exponent =  firstFavNum ** secondFavNum;
+console.log("exponent:", exponent);
